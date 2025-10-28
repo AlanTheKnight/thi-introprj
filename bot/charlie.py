90fafbdbb27721ddec9b7ea07d1e0fb33ba8107f
shakik19 <1ne9ine.o@gmail.com>
2025-10-28 11:53:35 +0100

    Revert "add patch for case in-sensitiveness"
    This reverts commit 6db031ee785075da539878733ff6a8d5838c7cc6.

--- bot/charlie.py
+++ bot/charlie.py
@@ -14,14 +14,12 @@ def init():
         "testBot",
         logic_adapters=[
             {
-                'import_path': 'case_insensitive_adapter.CaseInsensitiveBestMatch',
+                'import_path': 'chatterbot.logic.BestMatch',
                 'default_response': "I'm not sure about that yet.",
                 'maximum_similarity_threshold': 0.90
             }
         ],
-        preprocessors=[
-            'chatterbot.preprocessors.clean_whitespace'
-        ],
+        preprocessors=['chatterbot.preprocessors.clean_whitespace'],
         response_selection_method='chatterbot.response_selection.get_most_frequent_response',
         read_only=False
     )
